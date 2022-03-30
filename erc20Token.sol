bdf2afabdbe7b6d3aa8679d1f75361041b3b4758
Michelle David <57120467+michhub-dev@users.noreply.github.com>
2022-03-30 14:33:48 +0100

    Created erc20 token

---
+++ erc20Token.sol
@@ -0,0 +1,35 @@
+//state solidity version
+pragma solidity 0.5.1;
+
+contract ERC20Token {
+    string public name;
+    mapping(address => uint256) public balances;
+
+    function mint() public {
+        balances[tx.origin] ++;
+    }
+}
+
+contract MichContract {
+    address payable wallet;
+    address public token; 
+
+    constructor(
+        address payable _wallet,
+         address _token)
+         public
+         {
+             wallet = _wallet;
+             token = _token;
+         }
+
+         function() external payable {
+             buyToken();
+ 
+        }
+
+        function buyToken() public payable {
+             ERC20Token(address(token)).mint();
+            wallet.transfer(msg.value);
+        }
+}
